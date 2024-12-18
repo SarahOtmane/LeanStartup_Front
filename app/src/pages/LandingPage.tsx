0f0b9fa9310be2610a1c533310aa1fab13edd115
SarahOtmane <sarahotmane02@gmail.com>
2024-12-18 23:24:08 +0100

    ajout de la section 3

--- app/src/pages/LandingPage.tsx
+++ app/src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import HeaderLanding from '../components/landingPage/HeaderLanding';
 import Section1 from '../components/landingPage/Section1';
+import Section2 from '../components/landingPage/Section2';
 
 const LandingPage: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const LandingPage: React.FC = () => {
             <HeaderLanding />
             <main className='landingPage background-color padding'>
                 <Section1 />
+                <Section2 />
             </main>
         </>
     );
