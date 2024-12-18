7f6c3acc95d352af9986dfe22be3c05fdd6b20a8
SarahOtmane <sarahotmane02@gmail.com>
2024-12-18 20:23:24 +0100

    header de la landing page validé

--- app/src/components/HeaderLanding.tsx
+++ app/src/components/HeaderLanding.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Logo from '../assets/logo.png';
+import Button from './Button';
 
-import '../styles/components/header.style.css';
+import '../styles/components/headerLanding.style.css';
 
 const HeaderLanding: React.FC = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/');
+    }
+
     return (
-        <header className='Header row align-center'>
+        <header className='HeaderLanding background-color row align-center align-center justify-sp-between'>
             <img alt='logo Hive2Work' src={Logo} />
-            <nav>
+            <nav className='navHeader row'>
                 <ul className='row'>
                     <li><a href='#'>Services</a></li>
                     <li><a href='#'>À propos</a></li>
                     <li><a href='#'>FAQ</a></li>
                     <li><a href='#'>Newsletter</a></li>
                 </ul>
+                <Button 
+                    content='Accéder au site' 
+                    handleClick={handleClick}
+                    style='black'
+                />
+                <Button 
+                    content={`Télécharger l'application`}
+                    handleClick={handleClick}
+                    style='white'
+                />
             </nav>
         </header>
     );
