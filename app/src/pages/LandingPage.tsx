e545dde8154e9ec5634b41d792d3e47ea86f0ec0
SarahOtmane <sarahotmane02@gmail.com>
2024-12-18 16:48:45 +0100

    composant pour la landing page ( importation du header )

---
+++ app/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HeaderLanding from '../components/HeaderLanding';
+
+const LandingPage: React.FC = () => {
+    return (
+        <>
+            <HeaderLanding />
+        </>
+    );
+}
+
+export default LandingPage;
