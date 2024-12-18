71889cbc4e25dfbea12da5073ad1f3258b11d085
SarahOtmane <sarahotmane02@gmail.com>
2024-12-18 21:07:58 +0100

    import du composant section 1

--- app/src/pages/LandingPage.tsx
+++ app/src/pages/LandingPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import HeaderLanding from '../components/HeaderLanding';
+import HeaderLanding from '../components/landingPage/HeaderLanding';
+import Section1 from '../components/landingPage/Section1';
 
 const LandingPage: React.FC = () => {
     return (
         <>
             <HeaderLanding />
+            <main className='landingPage background-color padding'>
+                <Section1 />
+            </main>
         </>
     );
 }
