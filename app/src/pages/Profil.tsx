6db01e8ad7f4b4b645936ba8b5d7c17202b16fad
SarahOtmane <sarahotmane02@gmail.com>
2024-12-19 21:11:01 +0100

    ajouter le header au composant profil

--- app/src/pages/Profil.tsx
+++ app/src/pages/Profil.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../components/profil/Header';
 
 const Profil: React.FC = () =>{
     return(
-        <></>
+        <>
+            <Header />
+        </>
     )
 }
 
