18b671ddf7d450ffd337bbe821668b07798565cb
SarahOtmane <sarahotmane02@gmail.com>
2024-12-19 23:59:15 +0100

    ajout de la premiere section du composant profil

--- app/src/pages/Profil.tsx
+++ app/src/pages/Profil.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+
 import Header from '../components/profil/Header';
+
+import '../styles/pages/Profil.style.css';
+import CardPoste from '../components/profil/CardPoste';
 
 const Profil: React.FC = () =>{
     return(
         <>
             <Header />
+            <main className='profil background-color padding'>
+                <h1 className='text-uppercase text-align-center'>Bienvenue dans mon univers</h1>
+                <CardPoste />
+            </main>
         </>
     )
 }
