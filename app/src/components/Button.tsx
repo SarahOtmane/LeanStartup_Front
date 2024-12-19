c46ee294713264557742d0a0e253aed378033ad1
SarahOtmane <sarahotmane02@gmail.com>
2024-12-19 12:08:36 +0100

    update button's content

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import icon_fleche from '../assets/fleche_button.svg';
+import icon_profil from '../assets/icon_profil.svg';
 
 import '../styles/components/button.style.css';
 
@@ -9,12 +10,14 @@ interface ButtonProps {
     handleClick?: () => void;
     style?: string;
     classe?: string;
+    profil?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ content, handleClick, style, classe='' }) => {
+const Button: React.FC<ButtonProps> = ({ content, handleClick, style, classe='', profil=false }) => {
     return (
         <button onClick={handleClick} className={`row ${classe} ${style==='white' ? 'button_white' : 'button_black'}`}>
-            <img src={icon_fleche} alt='icon fleche' />
+            {!profil && <img src={icon_fleche} alt='icon fleche' />}
+            {profil && <img src={icon_profil} alt='icon fleche' />}
             <p>{content}</p>
         </button>
     );
