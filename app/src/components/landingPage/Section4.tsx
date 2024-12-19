0a2b9071cf9354eb7aa0798f8e482f3a336d0ad3
SarahOtmane <sarahotmane02@gmail.com>
2024-12-19 14:07:47 +0100

    update du composant section 4 + son style

--- app/src/components/landingPage/Section4.tsx
+++ app/src/components/landingPage/Section4.tsx
@@ -14,7 +14,7 @@ const Section4: React.FC = () => {
     return (
         <section className='section4'>
             <h2 className='sous-titre text-uppercase text-align-center'>Reste informé(e) avec notre newsletter</h2>
-            <p className='text-align-center'>Ne manque aucune opportunité pour booster ta recherche d’alternance et ta carrière. <br></br> Inscris-toi à notre newsletter et reçois directement dans ta boîte mail</p>
+            <p className='text-align-center margin-auto'>Ne manque aucune opportunité pour booster ta recherche d’alternance et ta carrière. <br></br> Inscris-toi à notre newsletter et reçois directement dans ta boîte mail</p>
             <input type='email' placeholder='Ton adresse mail' />
             <Button 
                 content={`Je m'abonne`}
