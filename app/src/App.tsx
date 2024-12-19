c2183e073fa0b3abdcf75cd2a112ac98fe79b870
SarahOtmane <sarahotmane02@gmail.com>
2024-12-19 12:08:21 +0100

    ajout du composant home a l app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import LandingPage from './pages/LandingPage';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<LandingPage />} />
+                <Route path='/home' element={<Home />} />
             </Routes>
         </Router>
     );
