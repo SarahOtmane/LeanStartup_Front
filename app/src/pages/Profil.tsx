18107dd2bcd372f28c20d4b3aa129a692cbd5e36
SarahOtmane <sarahotmane02@gmail.com>
2024-12-20 01:36:09 +0100

    add competences au composant profil

--- app/src/pages/Profil.tsx
+++ app/src/pages/Profil.tsx
@@ -6,6 +6,7 @@ import '../styles/pages/Profil.style.css';
 import CardPoste from '../components/profil/CardPoste';
 import Avis from '../components/profil/Avis';
 import FooterLanding from '../components/landingPage/FooterLanding';
+import Competences from '../components/profil/Competences';
 
 const Profil: React.FC = () =>{
     return(
@@ -14,6 +15,7 @@ const Profil: React.FC = () =>{
             <main className='profil background-color padding'>
                 <h1 className='text-uppercase text-align-center'>Bienvenue dans mon univers</h1>
                 <CardPoste />
+                <Competences />
                 <Avis />
             </main>
             <FooterLanding />
