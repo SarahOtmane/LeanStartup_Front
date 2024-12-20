2bdf9970a36f4d23d8fee8df523c971d027b5195
SarahOtmane <sarahotmane02@gmail.com>
2024-12-20 02:12:12 +0100

    composant competences + son css

--- app/src/components/profil/Competences.tsx
+++ app/src/components/profil/Competences.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { hardskills, softSkills, langues } from './competences.data';
 
+import '../../styles/components/profil/Competences.style.css';
+
 const Competences: React.FC = () =>{
     const [btnSelected, setBtnSelected] = useState('Compétences');
 
@@ -10,18 +12,18 @@ const Competences: React.FC = () =>{
     };
 
     return(
-        <section className='compet'>
+        <section className='compet row justify-sp-between'>
             <div className='competences'>
-                <div className='row justify-sp-between btn'>
+                <div className='btn row justify-sp-between btn'>
                     <article className={btnSelected==='Compétences' ? 'selected' : ''}><button className={`text-uppercase ${btnSelected==='Compétences' ? 'selected' : ''}`} onClick={selectBtn} id='Compétences'>Compétences</button></article>
                     <article className={btnSelected==='pourquoi' ? 'selected' : ''}><button className={`text-uppercase ${btnSelected==='pourquoi' ? 'selected' : ''}`} onClick={selectBtn} id='pourquoi'>Expériences</button></article>
                     <article className={btnSelected==='Formations' ? 'selected' : ''}><button className={`text-uppercase ${btnSelected==='Formations' ? 'selected' : ''}`} onClick={selectBtn} id='Formations'>Formations</button></article>
                 </div>
 
                 {btnSelected === 'Compétences' && (
-                    <div>
-                        <h3 className='text-uppercase'>Hardskills</h3>
-                        <article className='row justify-sp-between'>
+                    <div className='content'>
+                        <h3 className='text-uppercase color-white semi-bold'>Hardskills</h3>
+                        <article className='row'>
                             {hardskills.map((skill, index) => (
                                 <button key={index} className='skill row'>
                                     {skill.img}
@@ -29,16 +31,16 @@ const Competences: React.FC = () =>{
                                 </button>
                             ))}
                         </article>
-                        <h3 className='text-uppercase'>Softskills</h3>
-                        <article className='row justify-sp-between'>
+                        <h3 className='text-uppercase color-white semi-bold'>Softskills</h3>
+                        <article className='row'>
                             {softSkills.map((skill, index) => (
                                 <button key={index} className='skill row'>
                                     {skill}
                                 </button>
                             ))}
                         </article>
-                        <h3 className='text-uppercase'>Langues</h3>
-                        <article className='row justify-sp-between'>
+                        <h3 className='text-uppercase color-white semi-bold'>Langues</h3>
+                        <article className='row'>
                             {langues.map((langue, index) => (
                                 <button key={index} className='skill row'>
                                     {langue.img}
@@ -50,16 +52,16 @@ const Competences: React.FC = () =>{
                 )}
             </div>
             <div className='projets'>
-                <h3 className='text-uppercase'>Mes 3 projets incontournables</h3>
+                <h3 className='text-uppercase text-align-center'>Mes 3 projets incontournables</h3>
                 <article>
                     <div className='image'>
-                        <p>Virtual Urban Flow</p>
+                        <p className='color-white bold'>Virtual Urban Flow</p>
                     </div>
                     <div>
-                        <p>The Drip Grillz</p>
+                        <p className='color-white bold'>The Drip Grillz</p>
                     </div>
                     <div>
-                        <p>Memphis</p>
+                        <p className='color-white bold'>Memphis</p>
                     </div>
                 </article>
             </div>
